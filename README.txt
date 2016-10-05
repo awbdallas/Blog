4f6a2031d0c542ec5f0e0139472aa8d5b823fee4
Austin <awbdallas@gmail.com>
2016-10-05 08:47:46 -0500

    First commit. All from tutorial, everything past this point is stuff I've added

---
+++ README.txt
@@ -0,0 +1,22 @@
+Blog README
+==================
+About
+--------------
+
+The first commit is all from the pyramid_blogr tutorial. Anything past that is
+things I've added. 
+
+Link to said tutorial: http://docs.pylonsproject.org/projects/pyramid-blogr/en/latest/
+
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $VENV/bin/pip install -e .
+
+- $VENV/bin/initialize_pyramid_blogr_db development.ini
+
+- $VENV/bin/pserve development.ini
+
---
+++ pyramid_blogr/models/blog_record.py
@@ -0,0 +1,31 @@
+import datetime # default dates on mdels
+from pyramid_blogr.models.meta import Base  #sqlalchmy
+from sqlalchemy import (
+    Column,
+    Integer,
+    Unicode,
+    UnicodeText,
+    DateTime,
+)
+
+from webhelpers2.text import urlify # generate slugs
+from webhelpers2.date import distance_of_time_in_words # human friendly 
+
+class BlogRecord(Base):
+    __tablename__ = 'entries'
+    id = Column(Integer, primary_key=True)
+    title = Column(Unicode(255), unique=True, nullable=False)
+    body = Column(UnicodeText, default=u'')
+    created = Column(DateTime, default=datetime.datetime.utcnow)
+    edited = Column(DateTime, default=datetime.datetime.utcnow)
+
+    @property 
+    def slug(self):
+        return urlify(self.title)
+
+    
+    @property
+    def created_in_words(self):
+        return distance_of_time_in_words(self.created,
+                datetime.datetime.utcnow())
+
